0f424ac25fe6b7783774dc3116eb5f050796cca1
avillalobos <avillalobos@ZE-AVILLALOBOS>
2017-07-11 20:36:19 -0500

    Cambios nuevos:Agregando el Login.

---
+++ src/app/models/usuarioModel.ts
@@ -0,0 +1,82 @@
+﻿ export class UsuarioModel
+    {
+          idUsuario :string;
+
+         idCompania :string ;
+
+         idDivision :string;
+
+          idTipoUsuario :string;
+
+          nombre :string;
+
+          login :string;
+
+          password :string;
+
+          correo :string;
+
+          directorio :string;
+
+          flg_situacion :string;
+
+          flg_situacionb :boolean;
+
+          flg_contrasenia :string;
+
+          nombreCompania :string;
+
+          nombreDivision :string;
+
+          nombreTipoUsuario :string;
+
+          idRol :string;
+
+          directorioJs :string;
+
+          nombreRol :string;
+
+          codInterno :string;
+
+          nombrePersonal :string;
+
+          idPersonal :string;
+
+          idPersonalAnt :string;
+
+          isDomainUser :boolean;
+
+          ipMaquina :string;
+
+          hostMaquina :string;
+
+          idUsuarioIngreso :string;
+
+          rolesUsuario :string;
+
+         /*IEnumerable<CompaniaModel> lstCompania 
+
+         IEnumerable<DivisionModel> lstDivision 
+
+         IEnumerable<TipoUsuarioModel> lstTipoUsuario 
+
+         List<UsuarioDivisionModel> lstUsuarioDivision */
+
+          idCargoPers :string;
+
+          idDepartamento :string;
+
+          idDivisionAdmin :string;
+
+          divisionAdmin :string;
+
+          sessionKey :string;
+
+         fecIngreso :any;//date;
+
+          codAlmacen :string;
+
+          idAlmacen :string;
+
+          esVendedor :string;
+    }
---
+++ src/app/view/home/home.component.ts
@@ -0,0 +1,30 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { UsuarioModel } from 'app/models/index';
+import { UserService } from 'app/_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+    currentUser: UsuarioModel;
+    users: UsuarioModel[] = [];
+
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+}
